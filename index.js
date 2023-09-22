bd991f294edde352f9feb09a3800bbee3d920da3
DanniAdhyatma <dannirachman2@gmail.com>
2023-09-22 13:58:40 +0800

    Adding port env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const { error } = require('console');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   // Request handler goes here
